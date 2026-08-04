08a1c684e247297a2acf042aae88805ed138dfaf
abhinav550-lol <maabhinav550@gmail.com>
2026-08-04 15:32:48 +0530

    feat: getting started with generation service, planned out all the user stories for it

--- services/um_service/routes/authRoutes.js
+++ services/um_service/routes/authRoutes.js
@@ -2,6 +2,7 @@ import {Router} from 'express'
 import {authController} from '../controllers/authController.js'
 import {validate} from '../middleware/validateMiddleware.js'
 import {authValidator} from '../validators/authValidator.js'
+import {isLoggedIn} from '../middleware/authMiddlewares.js'
 
 const router = Router()
 
@@ -10,6 +11,6 @@ router.post('/login' , validate(authValidator.loginUserSchema) , authController.
 router.post('/initiate-register' , validate(authValidator.startUserRegistrationSchema) , authController.startUserRegistration);
 router.post('/register' , validate(authValidator.registerUserSchema) , authController.registerUser);
 
-router.get('/logout'  , authController.logoutUser)
+router.get('/logout'  , isLoggedIn , authController.logoutUser)
 
 export default router;
