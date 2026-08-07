22bcf1cc86691695c459c74a31a25a973b019c02
abhinav550-lol <maabhinav550@gmail.com>
2026-08-07 13:35:34 +0530

    feat: bootstrapped redirection service

---
+++ services/url_redirection_service/error/appError.js
@@ -0,0 +1,10 @@
+class appError extends Error{
+	constructor(statusCode, message){
+		super(message);
+		this.statusCode = statusCode;
+
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
+
+export {appError};
---
+++ services/url_redirection_service/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet"
+import cookieParser from "cookie-parser";
+import pinoHttp from "pino-http"
+import logger from "./utils/logger.js";
+import {appError} from "./error/appError.js";	
+import errorMiddleware from "./middleware/errorMiddleware.js";
+dotenv.config();
+const app = express();
+
+//Configuration
+app.use(cors({
+	origin: process.env.FRONTEND_URL,
+    credentials: true,
+}));
+app.use(express.json());
+app.use(helmet());
+app.use(cookieParser());
+app.use(
+  pinoHttp({
+    logger,
+  })
+);
+
+
+app.get("/health" , (req , res) => {
+	logger.info("Health check endpoint called");
+	res.status(200).json({ message: "URL redirection service is healthy" })
+})
+
+//Routes
+
+
+//Errors
+app.use(errorMiddleware);
+
+
+const PORT = process.env.PORT || 6003;
+app.listen(PORT , () => {
+	logger.info(`URL redirection service is running on port ${PORT}`);
+})
